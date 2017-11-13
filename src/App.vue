d6c8363473eaa7cd9dbc248764476cad3285ca58
Stanislaw Gutsch <kapitankapusta24@googlemail.com>
2017-11-13 03:28:51 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 footer-links">
+            <a href="#">imprint</a>
+            <a href="#">privacy</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style>
+#app {
+  color: #2c3e50;
+}
+  body {
+    margin: 0;
+  }
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueHead from 'vue-head'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../src/assets/styles/global.css'
+import '../src/assets/styles/fontello.css'
+import 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+// import * as THREE from 'three'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
+Vue.use(VueHead)

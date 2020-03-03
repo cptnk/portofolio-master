c55fdfece3559e4b038e9f627044826b0a165f42
Stani <stanislaw.gutsch@kreativrudel.de>
2020-03-03 11:04:05 +0100

    💄 added new hero animation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -41,82 +41,216 @@
                 </div>
             </div>
         </div>
+        <technologies></technologies>
+        <cv></cv>
         <script id="vertex-shader" type="x-shader/x-fragment">
        varying vec2 vUv;
 			void main()	{
 				vUv = uv;
 				gl_Position = vec4( position, 1.0 );
+
 			}
 
         </script>
         <script id="fragment-shader" type="x-shader/x-fragment">
-            varying vec2 vUv;
-			uniform float time;
-			uniform vec2 mouse;
-            uniform vec2 resolution;
+// enjoy
+// you can have it as a windows Demo exe File with Music
+//
+// skype: alien 5ive
 
-            const int layers = 16;
-            const float speed = 0.035;
+#ifdef GL_ES
+precision mediump float;
+#endif
+uniform float time;
+uniform vec2 resolution;
+#define iTime time
+#define iResolution resolution
+const vec4 iMouse = vec4(0.0);
 
-            float nrand (vec2 co) {
-                float a = fract(cos(co.x * 8.3e-3 + co.y) * 4.7e5);
-                float b = fract(sin(co.x * 0.3e-3 + co.y) * 1.0e5);
-                return a * .5 + b * .5;
-            }
+#define POSTPROCESS
+#define RAYMARCHING_STEP 35
+#define RAYMARCHING_JUMP 1.
 
-            float genstars (float starsize, float density, float intensity, vec2 seed)
-            {
-                float rnd = nrand(floor(seed * starsize));
-                float stars = pow(rnd,density) * intensity;
-                return stars;
-            }
-            vec3 White = vec3(1,1,1);
+const float PI = 3.14159265359;
+float snoise(vec3 v);
+float vmax(vec3 v) {return max(max(v.x, v.y), v.z);}
+float fBox(vec3 p, vec3 b) {
+	vec3 d = abs(p) - b;
+	return length(max(d, vec3(0))) + vmax(min(d, vec3(0)));
+}
+float fOpUnionRound(float a, float b, float r) {
+	vec2 u = max(vec2(r - a,r - b), vec2(0));
+	return max(r, min (a, b)) - length(u);
+}
 
-            float forest(float x) {
-                return abs(mod(x, 1.7) - 0.85);
-            }
+float map( in vec3 pos ) {
+    float time = iTime;
+    pos -= snoise(pos*0.1+time);
+	float d = -10. + pos.y + snoise(pos/41.+time)*10. + snoise(pos/10.+time)*3.+ snoise(pos/80.+time)*15.+ snoise(pos);
+    pos += snoise(pos+time)+snoise(pos*2.+time);
+    d = fOpUnionRound( d, fBox(pos-vec3(4.,10.,0.),vec3(10.5,9.,15.)), 6.);
+	return d;
+}
 
-            void main( void ) {
-            vec2 offset = vec2(time * 8.25,0);
 
-            vec2 star = 1.0 * (gl_FragCoord.xy / resolution) - 1.0;
-            star.y *= resolution.y / resolution.x;
-            star.y += time * speed;
+float castRay( in vec3 ro, in vec3 rd, inout float depth )
+{
+	float t = 0.0;
+	float res;
+	for( int i=0; i<RAYMARCHING_STEP; i++ )
+	{
+		vec3 pos = ro+rd*t;
+		res = map( pos );
+		if( res < 0.01 || t > 150. ) break;
+		t += res*RAYMARCHING_JUMP;
+		depth += 1./float(RAYMARCHING_STEP);
+	}
+	return t;
+}
 
-            star *= 150.0;
+float hash( float n ){
+	return fract(sin(n)*3538.5453);
+}
 
-            float intensity = genstars(10., 35., 2.5, star + offset);
-            intensity += genstars(5., 50., 12.5, star);
 
-            vec2 position = (gl_FragCoord.xy - resolution / 3.) / resolution.x;
-            position.x += time * speed;
+#ifdef POSTPROCESS
+vec3 postEffects( in vec3 col, in vec2 uv, in float time )
+{
+	// vigneting
+	col *= 0.7+0.3*pow( 16.0*uv.x*uv.y*(1.0-uv.x)*(1.0-uv.y), 0.1 );
+	return col;
+}
+#endif
 
-            float c = 1.2;
-            float k = 1.3;
+vec3 render( in vec3 ro, in vec3 rd, in vec2 uv )
+{
+	float depth = 0.;
+	float t = castRay(ro,rd,depth);
+    vec3 color = vec3(depth*uv.y,depth/5.,depth);
+    color += smoothstep(0.3,0.6,depth)*vec3(0.2,0.2,0.1);
+    color += smoothstep(0.6,1.,depth)*vec3(0.2,0.8,0.1);
+    return color;
+}
 
-            for (int j = 0; j < layers; j++)
-            {
-                float h = 0.;
-                float p = 1.5;
-                for (int i = 0; i < 20; i++)
-                {
-                p *= sin(position.x * 0.1) * 0.2 + 1.2;
-                h += abs(sin(p * 100.)) * (0.5*forest(position.x*p) - 0.25 - position.y / sqrt(k)) / p;
-                }
-                c = ceil(sign(h) * .5) * k;
-                if (c > 0.) break;
-                k -= 1.4 / float(layers);
-                position.x -= 10. + time * speed / float(layers);
-                position.y -= 0.1 / float(layers);
-            }
-            gl_FragColor = vec2(intensity + c, 21).xxxy;
-        }
+
+mat3 setCamera( in vec3 ro, in vec3 ta, float cr )
+{
+	vec3 cw = normalize(ta-ro);
+	vec3 cp = vec3(sin(cr), cos(cr),0.0);
+	vec3 cu = normalize( cross(cw,cp) );
+	vec3 cv = normalize( cross(cu,cw) );
+	return mat3( cu, cv, cw );
+}
+
+vec3 orbit(float phi, float theta, float radius)
+{
+	return vec3(
+		radius * sin( phi ) * cos( theta ),
+		radius * cos( phi ) + cos( theta ),
+		radius * sin( phi ) * sin( theta )
+	);
+}
+
+
+
+float mandelBrot(vec2 p) {
+	p.x = p.x * 3.5 - 2.5;
+	p.y = p.y * 2.0 - 1.0;
+
+	vec2 xy = vec2(0.);
+	int itter = 0;
+	const int maxItter = 128;
+
+	for(int i = 0; i < maxItter; i++) {
+		if (dot(xy, xy) > 4.) {
+			break;
+		}
+
+		float xtemp = dot(vec2(xy.x, -xy.y), xy) + p.x;
+		xy.y = 2.0 * xy.x * xy.y + p.y;
+		xy.x = xtemp;
+		itter++;
+	}
+
+	return float(itter) / float(maxItter);
+}
+
+void mainImage( out vec4 fragColor, in vec2 coords )
+{
+	float time = iTime;
+	vec2 uv = coords.xy / iResolution.xy;
+	vec2 mouse = iMouse.xy / iResolution.xy;
+	vec2 q = coords.xy/iResolution.xy;
+	vec2 p = -1.0+2.0*q;
+	p.x *= iResolution.x/iResolution.y;
+
+	//Camera
+	float radius = 60.;
+	vec3 ro = orbit(PI/2.-.5,PI/2.+sin(time)*.35,radius);
+	vec3 ta  = vec3(0.0, 0., 0.0);
+	mat3 ca = setCamera( ro, ta, 0. );
+	vec3 rd = ca * normalize( vec3(p.xy,1.2) );
+
+	vec3 color = render( ro, rd, uv );
+	#ifdef POSTPROCESS
+	color = postEffects( color, uv, time );
+	#endif
+	fragColor = vec4(color,1.0);
+}
+
+
+lowp vec4 permute(in lowp vec4 x){return mod(x*x*34.+x,289.);}
+lowp float snoise(in mediump vec3 v){
+  const lowp vec2 C = vec2(0.16666666666,0.33333333333);
+  const lowp vec4 D = vec4(0,.5,1,2);
+  lowp vec3 i  = floor(C.y*(v.x+v.y+v.z) + v);
+  lowp vec3 x0 = C.x*(i.x+i.y+i.z) + (v - i);
+  lowp vec3 g = step(x0.yzx, x0);
+  lowp vec3 l = (1. - g).zxy;
+  lowp vec3 i1 = min( g, l );
+  lowp vec3 i2 = max( g, l );
+  lowp vec3 x1 = x0 - i1 + C.x;
+  lowp vec3 x2 = x0 - i2 + C.y;
+  lowp vec3 x3 = x0 - D.yyy;
+  i = mod(i,289.);
+  lowp vec4 p = permute( permute( permute(
+	  i.z + vec4(0., i1.z, i2.z, 1.))
+	+ i.y + vec4(0., i1.y, i2.y, 1.))
+	+ i.x + vec4(0., i1.x, i2.x, 1.));
+  lowp vec3 ns = .142857142857 * D.wyz - D.xzx;
+  lowp vec4 j = -49. * floor(p * ns.z * ns.z) + p;
+  lowp vec4 x_ = floor(j * ns.z);
+  lowp vec4 x = x_ * ns.x + ns.yyyy;
+  lowp vec4 y = floor(j - 7. * x_ ) * ns.x + ns.yyyy;
+  lowp vec4 h = 1. - abs(x) - abs(y);
+  lowp vec4 b0 = vec4( x.xy, y.xy );
+  lowp vec4 b1 = vec4( x.zw, y.zw );
+  lowp vec4 sh = -step(h, vec4(0));
+  lowp vec4 a0 = b0.xzyw + (floor(b0)*2.+ 1.).xzyw*sh.xxyy;
+  lowp vec4 a1 = b1.xzyw + (floor(b1)*2.+ 1.).xzyw*sh.zzww;
+  lowp vec3 p0 = vec3(a0.xy,h.x);
+  lowp vec3 p1 = vec3(a0.zw,h.y);
+  lowp vec3 p2 = vec3(a1.xy,h.z);
+  lowp vec3 p3 = vec3(a1.zw,h.w);
+  lowp vec4 norm = inversesqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
+  p0 *= norm.x;
+  p1 *= norm.y;
+  p2 *= norm.z;
+  p3 *= norm.w;
+  lowp vec4 m = max(.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.);
+  return .5 + 12. * dot( m * m * m, vec4( dot(p0,x0), dot(p1,x1),dot(p2,x2), dot(p3,x3) ) );
+}void main(void)
+{
+    mainImage(gl_FragColor, gl_FragCoord.xy);
+}
         </script>
     </div>
 </template>
 
 <script>
   import * as THREE from 'three';
+  import Technologies from './Technologies.vue';
+  import Cv from './Cv.vue';
 
   export default {
     name: 'HelloWorld',
@@ -169,7 +303,10 @@
       this.$nextTick(function () {
         animate();
       });
-    }
+    },
+      components: {
+          Technologies, Cv
+      }
   }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Cv from '@/components/Cv'
+import Technologies from '@/components/Technologies'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +16,10 @@ export default new Router({
         path: '/cv',
         name: 'Cv',
         component: Cv
-    }
+    }, {
+          path: '/technologies',
+          name: 'Technologies',
+          component: Technologies
+      }
   ]
 })

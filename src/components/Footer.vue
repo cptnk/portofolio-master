ef4745f3403bd5c37845876a0d756c0571db1460
Stanislaw Gutsch <kapitankapusta24@googlemail.com>
2023-10-16 23:36:45 +0200

    deploy

---
+++ src/components/Footer.vue
@@ -0,0 +1,118 @@
+<template>
+    <div v-bind:class="mode === 'light' ? 'light_drop' : ''" class="back-drop">
+        <figure class="figure-container" v-if="mode !== 'light'">
+            <div class="figure_box yellow"></div>
+            <div class="figure_box purple"></div>
+            <div class="figure_box teal"></div>
+        </figure>
+        <footer class="container" id="cvFooter">
+            <div class="row flex-row flex-row-justify footer-links">
+                <a href="/">Home</a> <a href="/#/cv">Curriculum Vitae</a> <a href="/#/imprint">Imprint</a>
+            </div>
+        </footer>
+        <div class="copyright">©{{year}} cptnk</div>
+    </div>
+</template>
+
+<style scoped>
+    .back-drop {
+        position: relative;
+        background-color: #2c3e50;
+        padding-top: 7rem;
+        padding-bottom: 7rem;
+    }
+    .light_drop {
+        padding-top: 2rem;
+        padding-bottom: 3.5rem;
+        background-color: #fff;
+        border-top: 1px solid #2c3e50;
+    }
+
+    #cvFooter {
+        transform: translateY(0);
+    }
+    .footer-links {
+        gap: 1rem;
+    }
+    .copyright {
+        position: absolute;
+        bottom: 1rem;
+        left: 50%;
+        transform: translateX(-50%);
+        color: #fff;
+    }
+    .light_drop .copyright {
+        color: #2c3e50;
+    }
+    .figure-container {
+        position: absolute;
+        top: 0;
+        left: 2rem;
+        height: 14rem;
+        width: 300px;
+    }
+
+    @keyframes tilt-n-move-shaking {
+        0% { transform: translate(0, 0) rotate(45deg); }
+        25% { transform: translate(5px, 15px) rotate(45deg); }
+        50% { transform: translate(5px, 10px) rotate(45deg); }
+        75% { transform: translate(-5px, -15px) rotate(45deg); }
+        100% { transform: translate(0, 0) rotate(45deg); }
+    }
+
+    @keyframes tilt-n-move-shaking-purple {
+        0% { transform: translate(0, 0) rotate(45deg); }
+        25% { transform: translate(3px, 12px) rotate(45deg); }
+        50% { transform: translate(3px, 7px) rotate(45deg); }
+        75% { transform: translate(-3px, -12px) rotate(45deg); }
+        100% { transform: translate(0, 0) rotate(45deg); }
+    }
+
+    @keyframes tilt-n-move-shaking-teal {
+        0% { transform: translate(0, 0) rotate(45deg); }
+        25% { transform: translate(5px, 15px) rotate(45deg); }
+        50% { transform: translate(5px, 10px) rotate(45deg); }
+        75% { transform: translate(-5px, -15px) rotate(45deg); }
+        100% { transform: translate(0, 0) rotate(45deg); }
+    }
+
+    .figure_box {
+        transform: rotate(45deg);
+        width: 200px;
+        height: 50px;
+        filter: drop-shadow(0 0 0.15rem black);
+    }
+
+    .yellow {
+        background-color: #ffff00;
+        animation: tilt-n-move-shaking ease-in-out 15s infinite;
+    }
+
+    .purple {
+        background-color: #ff00ff;
+        animation: tilt-n-move-shaking-purple ease-in 15s infinite;
+    }
+
+    .teal {
+        background-color: #00ffff;
+        animation: tilt-n-move-shaking-teal ease-out 15s infinite;
+    }
+
+    @media (max-width: 992px) {
+        .figure-container {
+            display: none;
+        }
+    }
+</style>
+
+<script>
+export default {
+    name: 'Footer',
+    props: ['mode'],
+    data () {
+        return {
+            year: new Date().getFullYear()
+        }
+    }
+}
+</script>
---
+++ src/components/Imprint.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="container">
+    <div class="row back-row">
+      <div class="col-12">
+        <a style="font-weight: bold" href="/"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+        </svg> home</a>
+      </div>
+    </div>
+    <div class="row imprint-section">
+      <div class="col-12">
+        <div class='impressum'><h1>Impressum</h1><p>Angaben gemäß § 5 TMG</p><p>Stanislaw Gutsch <br>
+          Vulkanstr. 23<br>
+          53179 Bonn <br>
+        </p><p> <strong>Vertreten durch: </strong><br>
+          Stanislaw Gutsch<br>
+        </p><p><strong>Kontakt:</strong> <br>
+          E-Mail: <a href='mailto:stanislaw.gutsch@gmail.com'>stanislaw.gutsch@gmail.com</a></br></p><p><strong>Haftungsausschluss: </strong><br><br><strong>Urheberrecht</strong><br><br>
+          Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p><br>
+          Impressum vom <a href="https://www.impressum-generator.de">Impressum Generator</a> der <a href="https://www.kanzlei-hasselbach.de/">Kanzlei Hasselbach, Rechtsanwälte für Arbeitsrecht und Familienrecht</a>
+        </div>
+      </div>
+    </div>
+    <Footer mode="light"></Footer>
+  </section>
+</template>
+<script>
+import Footer from './Footer.vue'
+export default {
+  name: 'Imprint',
+  components: {Footer},
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
+<style scoped>
+  .back-row {
+    margin-top: 2rem;
+  }
+  .imprint-section {
+    margin-top: 5rem;
+  }
+</style>
